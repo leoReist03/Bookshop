6e3255ae9eacae6dd0e0a2827d51838a2f8d42ef
leoReist <leo.reist@gmx.ch>
2024-08-14 12:08:56 +0200

    minor design changes

--- frontend/app/ui/skeletons.tsx
+++ frontend/app/ui/skeletons.tsx
@@ -3,12 +3,17 @@ import styles from '@/app/ui/cssModules/shimmer.module.css'
 export function TableSkeleton() {
     return (
         <div className={`${styles.shimmer} w-full text-center bg-icewhite p-3 rounded-lg mb-5`}>
+            <div className="flex items-center justify-between gap-2 mb-2">
+                <div className='rounded-md bg-white text-white py-1 w-2/5'>Search</div>
+                <div className='rounded-md bg-cyan text-cyan py-1 w-1/6'>Create</div>
+            </div>
             <div className="rounded-t-lg w-full bg-cyan mg-h-fit text-cyan">Table head</div>
             <div className="w-full bg-white text-white">Table Row</div>
             <div className="w-full bg-gray-100 text-gray-100">Table Row</div>
             <div className="w-full bg-white text-white">Table Row</div>
             <div className="w-full bg-gray-100 text-gray-100">Table Row</div>
-            <div className="rounded-b-lg w-full bg-white text-white">Table Row</div>
+            <div className="w-full bg-white text-white">Table Row</div>
+            <div className='rounded-b-lg bg-cyan text-cyan'>Pagination</div>
         </div>
     );
 }
